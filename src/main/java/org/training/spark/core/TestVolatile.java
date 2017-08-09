2f135966ee311c226e9f73a730c7d8d8004e0aa6
studysai <studysai@sina.com>
2017-08-09 14:47:21 +0800

    sparkTest

---
+++ src/main/java/org/training/spark/core/TestVolatile.java
@@ -0,0 +1,29 @@
+package org.training.spark.core;
+
+class ThreadDemo implements Runnable {
+
+    private volatile boolean flag = false;
+
+    @Override
+    public void run() {
+
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+        }
+
+        flag = true;
+
+        System.out.println("flag=" + isFlag());
+
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+}
